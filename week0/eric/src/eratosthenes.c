734bbfb9e35e9abe97580c3da93c61f2598b3e8b
Eric Andow <eandow16@csl04-l.cornellcollege.edu>
2015-11-23 14:33:41 -0600

    Fill in C code

--- week0/eric/src/eratosthenes.c
+++ week0/eric/src/eratosthenes.c
@@ -1,6 +1,5 @@
-
 // eratosthenes.c
-// Norbert Wiener
+// Eric Andow
 // CSC311 Systems Software
 // 23 November 2015
 
@@ -9,34 +8,63 @@
 
 // this program will identify all
 // prime numbers that are < SIZE
+#define SIZE 1000000 //1 million
+
+int bitmap[SIZE];
 
 // filter() executes the Sieve of Eratosthenes
 // algorithm
+void filter() {
+  int i;
+  int j = 2;
   
   // initialize bitmap (array of zeroes and ones)
+  // one denotes composite, 0 denotes prime.
   // 0 and 1 are not prime
   // begin by assuming that any integer >= 2 could
   // be prime
+  bitmap[0] = 1;
+  bitmap[1] = 1;
 
+  for(i = 2; i < SIZE; i++) {
     // bitmap[i] = 0 means i could be prime
     // (the next loop will determine if i is really prime)
-
-
-
+    bitmap[i] = 0;
+  } //for
+    
   // 2 is the smallest prime number so
   // start the search for prime numbers
   // at 2
+  i = 2;
 
+  while(i < SIZE) {
+    if(bitmap[i] == 0) {
       // if i is prime, then all of its 
       // multiples are composite (not prime)
+      for(j = i+i; j < SIZE; j+= i) {
+        bitmap[j] = 1;
+      } //for
+    } //if
+
+    i++;
+  } //while
+}
 
 
 int main( int argc, char** argv ) {
   // find all of the prime numbers < SIZE
+  filter();
 
   // print all of the prime numbers < SIZE
+  int i;
 
-  printf( "Hello from eratosthenes!\n" );
+  for(i = 0; i < SIZE; i++) {
+    if(bitmap[i] == 0) {
+      printf("%d ", i);
+    }
+  }
+
+  printf( "\n" );
 
   exit(0);
 } // main( int, char** )
--- week0/eric/src/treesort.c
+++ week0/eric/src/treesort.c
@@ -1,6 +1,5 @@
-
 // treesort.c
-// Norbert Wiener
+// Eric Andow
 // CSC311 Systems Software
 // 23 November 2015
 
@@ -8,30 +7,88 @@
 #include <stdlib.h>
 
 // define a node in a binary search tree
+struct node {
+  int value;
+  struct node* lp;
+  struct node* rp;
+};
 
 // define aliases
+typedef struct node Node;
+typedef struct node* NodePointer;
 
 // define a function that will create
 // a new tree that contains only a single
 // node
+NodePointer createRoot( int n ){
+  NodePointer np = (NodePointer) malloc( sizeof(Node) );
+  np->value = n;
+  np->lp = NULL;
+  np->rp = NULL;
+  return np;
+} //createRoot (int)
 
 // define a function that will add a new
 // node to an existing binary search tree
+NodePointer addNode( NodePointer np, int n ) {
+  NodePointer rp = np;
 
+  if(np == NULL) {
+    rp = createRoot(n);
+  } //if
+  else if( n < np->value ) {
+    if( np->lp == NULL ) {
+      np->lp = createRoot(n);
+    } //if
+    else {
+      np->lp = addNode( np->lp, n );
+    } //else
+  } //else if
+  else if( n > np->value ) {
+    if( np->rp == NULL) {
+      np->rp = createRoot(n);
+    } //if
+    else {
+      np->rp = addNode( np->rp, n);
+    } //else
+  } //else if
+  else {
     // if value to be added is already in tree, do nothing
     // (do not add duplicate values to tree)
+  } //else
+
+  return rp;
+} //addNode(NodePointer, int)
 
 // traverse a binary search tree
 // in order, printing the values
 // found at each node
+void printNodes( NodePointer np ) {
+  if(np != NULL) {
+    printNodes(np->lp);
+    printf("value at node = %4d\n", np->value);
+    printNodes(np->rp);
+  } //if
+}
 
 int main( int argc, char** argv ) {
 
   // build a binary search tree that
   // contains integers found on the command line,
   // then print the integers in ascending order
+  if(argc > 1) {
 
-  printf( "Hello from treesort!\n" );
+    NodePointer rp = createRoot( atoi(argv[1]) );
+
+    int i;
+    for(i = 2; i < argc; i++) {
+      rp = addNode( rp, atoi(argv[i]) );
+    }//for
+
+    printNodes(rp);
+  } //if
+
+  printf( "\n" );
 
   exit(0);
 } // main( int, char** )
